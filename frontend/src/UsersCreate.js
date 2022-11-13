04ce949482c91a848b2cb747f0095b30ddee877d
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2022-11-13 12:20:13 -0300

    Corrigindo dependência não utilizada

--- frontend/src/UsersCreate.js
+++ frontend/src/UsersCreate.js
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
